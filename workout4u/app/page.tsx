8e07dbe2c013abdb6cfb40f33b2190d010fe8e52
echiewe <126719218+echiewe@users.noreply.github.com>
2025-11-03 22:12:30 -0700

    Initial frontend

--- workout4u/app/page.tsx
+++ workout4u/app/page.tsx
@@ -2,8 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="">
-            
+        <div className="bg-[url(/svgs/background.svg)] w-full h-screen bg-cover bg-top">
+            <div className="w-full h-full flex justify-center items-center">
+                <div className="w-[400px] h-[500px] bg-perfume/70 pixel-border flex flex-col items-center justify-around pb-5">
+                    <h1 className="text-shadow-lg">WORKOUT4U</h1>
+                    <section className="w-3/4 h-3/4 bg-perfume pixel-border">
+                        form goes here
+                    </section>
+                </div>
+            </div>
         </div>
     );
 }
